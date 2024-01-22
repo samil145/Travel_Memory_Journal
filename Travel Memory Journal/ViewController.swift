77af40de4c3bfabe34edf9af7bf2846c9e28230f
samil145 <samil.bayramli.sb@gmail.com>
2024-01-23 01:03:49 +0400

    Initial Commit

---
+++ Travel Memory Journal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Travel Memory Journal
+//
+//  Created by Shamil Bayramli on 23.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
